61deca35e6c9a12e5a291cd5983a69e08c4c4388
PG <gamper74@gmail.com>
2023-05-31 15:52:00 +0500

    2023.05.31 changed

--- include/search.h
+++ include/search.h
@@ -9,22 +9,13 @@
 #include "hash.h"
 
 struct File {
-    std::string file_path;
-    std::ifstream file_content;
-    std::vector<std::string> hash;
-    size_t file_sz;
-    size_t block_sz;
-    size_t block_qty;
-    std::unique_ptr<IHasher> hasher;
-    bool duplicate = false;
-
-    File(std::string path, size_t sz, size_t block_sz, std::string hash):
-            file_path(std::move(path)),
-            file_sz(sz),
-            block_sz(block_sz),
-            block_qty((sz + block_sz - 1) / block_sz),
-            file_content(file_path, std::ios::binary) {
-        set_hasher(hash);
+    File(std::string path_, size_t sz, size_t block_sz, std::string hash):
+        path(std::move(path_)),
+        size(sz),
+        block_sz(block_sz),
+        block_qty((sz + block_sz - 1) / block_sz),
+        content(path, std::ios::binary) {
+        hash_alg(hash);
         hash.reserve(block_qty);
     }
 
@@ -32,12 +23,12 @@ struct File {
         if (duplicate ^ other.duplicate) {
             return other.duplicate < duplicate;
         }
-       // return duplicate && hash[0] < other.hash[0];
-        return duplicate && file_sz < other.file_sz;
+        // return duplicate && hash_blocks[0] < other.hash_blocks[0];
+        return duplicate && size < other.size;
     }
 
     void equal(File &other) {
-        if (other.duplicate || file_sz != other.file_sz) return;
+        if (other.duplicate || size != other.size) return;
 
         for (size_t i = 0; i < block_qty; ++i) {
             if (hash_calc(i) != other.hash_calc(i)) return;
@@ -47,25 +38,35 @@ struct File {
     }
 
     std::string hash_calc(size_t i) {
-        if (hash.size() <= i) {
+        if (hash_blocks.size() <= i) {
             std::string block(block_sz, '\0');
-            file_content.seekg(i * block_sz);
-            file_content.read(&block[0], block_sz);
+            content.seekg(i * block_sz);
+            content.read(&block[0], block_sz);
 
-            hash.push_back(hasher->Hash(block.c_str(), block.size()));
+            hash_blocks.push_back(algorithm->Hash(block.c_str(), block.size()));
         }
-        return hash[i];
+        return hash_blocks[i];
     }
 
-    void set_hasher(const std::string &hash_) {
+    void hash_alg(const std::string &hash_) {
         if (hash_ == "md5")
-            hasher = std::make_unique<MD5Hasher>();
+            algorithm = std::make_unique<MD5Hasher>();
         else if (hash_ == "sha1")
-            hasher = std::make_unique<SHA1Hasher>();
+            algorithm = std::make_unique<SHA1Hasher>();
         else
-            hasher = std::make_unique<CRC32Hasher>();
+            algorithm = std::make_unique<CRC32Hasher>();
     }
 
+    std::string path;
+    std::vector<std::string> hash_blocks;
+    size_t size;
+    bool duplicate = false;
+
+private:
+    std::ifstream content;
+    size_t block_sz;
+    size_t block_qty;
+    std::unique_ptr<IHasher> algorithm;
 };
 
 namespace fs = std::filesystem;
@@ -80,8 +81,11 @@ struct Search {
            size_t depth) : file_min_sz(file_min_sz),
                            block_sz(block_sz),
                            depth(depth),
-                           hash(std::move(hash)){
-        scan_dirs(dir_scan, dir_skip, masks);
+                           hash(std::move(hash)),
+                           file_masks(masks),
+                           dir_scan(dir_scan),
+                           dir_skip(dir_skip){
+        scanner();
         find_duplicate();
         print();
     }
@@ -92,27 +96,30 @@ private:
     size_t block_sz;
     size_t depth;
     std::string hash;
+    const std::vector<std::string> &dir_scan;
+    const std::vector<std::string> &dir_skip;
+    const std::vector<std::string> &file_masks;
     std::vector<File> file_list;
-    std::vector<std::string> file_masks;
 
-
-    void scan_dirs(const std::vector<std::string> &dir_scan, const std::vector<std::string> &dir_skip,
-                   const std::vector<std::string> &masks) {
-        for (const auto &d: dir_scan) {
-            if (fs::exists(d)
-                && std::find(dir_skip.begin(), dir_skip.end(), d) == std::end(dir_skip)) {
-                select_files(d, masks);
+    void scanner() {
+        for(const auto &dir: dir_scan) {
+            if(dir_no_skip(dir)) {
+                search_files(dir);
             }
         }
     }
 
-    void select_files(const std::string &dir, const std::vector<std::string> &mask) {
+    bool dir_no_skip(const std::string & dir){
+        return (fs::exists(dir) && std::find(dir_skip.begin(), dir_skip.end(), dir) == std::end(dir_skip));
+    }
+
+    void search_files(const std::string &dir) {
         for (auto it = fs::recursive_directory_iterator(dir); it != fs::recursive_directory_iterator(); ++it) {
             if (!depth)
                 it.disable_recursion_pending();
 
             if (it->is_regular_file() && it->file_size() > file_min_sz) {
-                if (mask_match(it->path().string(), mask))
+                if (mask_match(it->path().string(), file_masks))
                     file_list.emplace_back(it->path().string(), it->file_size(), block_sz, hash);
             }
         }
@@ -145,9 +152,9 @@ private:
         for(int i = 0; i < file_list.size(); ++i) {
             const auto &f = file_list[i];
             if(!f.duplicate) break;
-            if(i > 0 && (f.hash[0] != file_list[i - 1].hash[0] || f.file_sz != file_list[i - 1].file_sz))
+            if(i > 0 && (f.hash_blocks[0] != file_list[i - 1].hash_blocks[0] || f.size != file_list[i - 1].size))
                 std::cout << '\n';
-            std::cout << f.file_path << " " << f.file_sz << " " << f.hash[0] << '\n';
+            std::cout << f.path << " " << f.size << " " << f.hash_blocks[0] << '\n';
         }
     }
 
